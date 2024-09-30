1f7befd77705322d056b73c6d980dc0c0cc272ad
moinkazi123 <155711429+moinkazi123@users.noreply.github.com>
2024-09-30 18:41:07 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Change to the application directory
 cd /home/ec2-user/WMSCOREAPIMob2
-dotnet ./WMSCOREAPI.dll --urls "http://*:5100;http://localhost:5101" &
+
+# Run the application using nohup
+nohup dotnet ./WMSCOREAPI.dll --urls "http://*:5100;http://localhost:5101" > app.log 2>&1 &
+
+# Display the process ID of the application
+echo "WMSCOREAPI is running with PID: $!"
